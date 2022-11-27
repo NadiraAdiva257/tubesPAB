65586439ca412563296b49d91947ff2d9c6a8567
anjeliputeri <anjeliputeri92@gmail.com>
2022-11-27 21:20:26 +0700

    add navbar

--- app/src/main/java/com/example/tubes/MainActivity.kt
+++ app/src/main/java/com/example/tubes/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.tubes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var goHome : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        goHome = findViewById<Button>(R.id.button)
+
+        goHome.setOnClickListener{
+            var home = Intent(this, Home::class.java)
+            startActivity(home)
+        }
     }
 }
