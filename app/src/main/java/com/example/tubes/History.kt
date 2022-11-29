30eaddf631c12bba48cdc09fb2084ef965abb51a
anjeliputeri <anjeliputeri92@gmail.com>
2022-11-29 13:59:21 +0700

    update color

---
+++ app/src/main/java/com/example/tubes/History.kt
@@ -0,0 +1,15 @@
+package com.example.tubes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+
+class History : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+        setContentView(R.layout.activity_home)
+    }
+}
