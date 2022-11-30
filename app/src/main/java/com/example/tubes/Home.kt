730763f3344af2346da6b2c5d01fd3355e41ce67
anjeliputeri <anjeliputeri92@gmail.com>
2022-11-30 16:19:29 +0700

    update

--- app/src/main/java/com/example/tubes/Home.kt
+++ app/src/main/java/com/example/tubes/Home.kt
@@ -1,15 +1,39 @@
 package com.example.tubes
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.FillEventHistory
 import android.view.View
 import android.widget.ImageView
 
 class Home : AppCompatActivity() {
+    lateinit var home: ImageView
+    lateinit var history: ImageView
+    lateinit var quest: ImageView
+    lateinit var notifikasi: ImageView
+    lateinit var profile: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         actionBar?.hide()
         setContentView(R.layout.activity_home)
+
+        home = findViewById<ImageView>(R.id.ic_home)
+        history = findViewById<ImageView>(R.id.ic_history)
+        quest = findViewById<ImageView>(R.id.ic_quest)
+        notifikasi = findViewById<ImageView>(R.id.ic_notifikasi)
+        profile = findViewById<ImageView>(R.id.ic_profile)
+
+
+
+
+
+
+
+
+
+
     }
 }
