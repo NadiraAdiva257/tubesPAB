a1aa6c6e97fb919cf368b2419e9beac5aed4faf3
anjeliputeri <anjeliputeri92@gmail.com>
2022-12-08 16:24:51 +0700

    upload splash screen and login

--- app/src/main/java/com/example/tubes/SplashScreenActivity.kt
+++ app/src/main/java/com/example/tubes/SplashScreenActivity.kt
@@ -3,23 +3,25 @@ package com.example.tubes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    lateinit var goHome : Button
+class SplashScreenActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         actionBar?.hide()
-        setContentView(R.layout.activity_main)
 
-        goHome = findViewById<Button>(R.id.button)
+        setContentView(R.layout.activity_splash_screen)
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
 
-        goHome.setOnClickListener{
-            var home = Intent(this, Home::class.java)
-            startActivity(home)
-        }
     }
 }
